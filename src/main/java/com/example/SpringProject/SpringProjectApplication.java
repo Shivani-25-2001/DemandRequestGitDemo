9dede7122ef58280c19dc8a45e96f5a909e4fa18
Shivani-25-2001 <shivanigurram2001@gmail.com>
2023-10-02 13:54:11 +0530

    added console of feature-2

--- src/main/java/com/example/SpringProject/SpringProjectApplication.java
+++ src/main/java/com/example/SpringProject/SpringProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("feature -2 added sucessfully");
 		SpringApplication.run(SpringProjectApplication.class, args);
 		System.out.println("feature -1 added sucessfully");
 	}
